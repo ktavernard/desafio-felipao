87c4000149950292126d0429ad301d50fb280e48
ktavernard-dio <ka.tavernard@gmail.com>
2024-01-25 21:42:02 -0300

    meu segundo

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-ola 
+let nomeHeroi = "Goku"
+let xpHeroi = 15.000
+
+if(xpHeroi > 10.001){
+    console.log("Radiante")
+
+}else if(xpHeroi => 9.001 < 10.000){
+    console.log("Imortal")
+
+}else{
+    console.log("Ascendente")
+}    
+
+if(xpHeroi = "Radiante"){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + xpHeroi)
+}
